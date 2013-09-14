5894db2fbd967d5e93d0fa9148fc6fcebb3dbc39
Martin Vejmelka <vejmelkam@gmail.com>
2013-09-13 18:56:51 -0600

    job now runs with periodic readouts from the history file with the python script psrc/process-frame.py

--- psrc/process-frame.py
+++ psrc/process-frame.py
@@ -3,10 +3,22 @@
 import netCDF4
 import sys
 import string
+import pytz
+from datetime import datetime
 
-d = netCDF4.Dataset("wksp/25b55327-2c43-4d62-beb9-a314ccf91c9f/wrf/wrfout_d01_2013-09-01_04:00:00")
-times = d.variables["Times"][:]
+def load_times(d):
+    gmt_tz = pytz.timezone('GMT')
+    tm = d.variables['Times'][:,...]
+    tp = []
+    for t in tm:
+        dt = datetime.strptime(''.join(t), '%Y-%m-%d_%H:%M:%S')
+        dt = dt.replace(tzinfo = gmt_tz)
+        tp.append(dt)
+    
+    return tp
+
+d = netCDF4.Dataset("wksp/25b55327-2c43-4d62-beb9-a314ccf91c9f/wrf/wrfout_d01_2013-09-01_03:00:00")
+times = load_times(d)
 if len(times) > 0:
-  print("The last ESMF timestamp in the file is %s" % string.join(times[-1], ''))
-
+  print("The last ESMF timestamp in the file is %s" % str(times[-1]))
 
