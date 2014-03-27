9a4af269d0171d2937525011f6fee4e873aa2962
Martin Vejmelka <vejmelkam@gmail.com>
2014-03-27 13:41:44 -0600

    startnode now uses JOXA_HOME env var

--- startnode.sh
+++ startnode.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 HOST=`hostname -s`
-erl -sasl -detached -sname wrfx2@$HOST -pa $HOME/Packages/joxa/ebin ebin deps/*/ebin -s wrfx2
+erl -sasl -detached -sname wrfx2@$HOST -pa $JOXA_HOME/ebin ebin deps/*/ebin -s wrfx2
 
