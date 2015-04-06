92239cb01e6b6adfc65701dce3357e72de4b2ae7
Martin Vejmelka <vejmelkam@gmail.com>
2015-04-06 16:47:59 -0600

    added joxa path to startwrfx2.sh script

--- startwrfx2.sh
+++ startwrfx2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 HOST=`hostname -s`
-erl -sasl -sname wrfx2@$HOST -pa ebin deps/*/ebin -s wrfx2
+erl -sasl -sname wrfx2@$HOST -pa ebin deps/*/ebin $HOME/Packages/joxa/ebin -s wrfx2
 
