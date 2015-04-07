871e14f56fdf948512cd533aeaa240e32497999f
Martin Vejmelka <vejmelkam@gmail.com>
2015-04-07 17:43:42 -0600

    modified startup scripts to route kernel and SASL logging to appropriate files in the log/ directory

--- startnode.sh
+++ startnode.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 HOST=`hostname -s`
-erl -sasl -detached -sname wrfx2@$HOST -pa ebin deps/*/ebin -s wrfx2
+erl \
+ -sasl sasl_error_logger '{file,"log/system.log"}' \
+ -detached \
+ -sname wrfx2@$HOST \
+ -pa ebin deps/*/ebin \
+ -s wrfx2
 
--- startwrfx2.sh
+++ startwrfx2.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 HOST=`hostname -s`
-erl -sasl -sname wrfx2@$HOST -pa ebin deps/*/ebin $HOME/Packages/joxa/ebin -s wrfx2
+erl \
+ -boot start_sasl \
+ -sasl sasl_error_logger tty \
+ -kernel error_logger tty \
+ -sname wrfx2@$HOST \
+ -pa ebin deps/*/ebin $HOME/Packages/joxa/ebin \
+ -s wrfx2 start
 
